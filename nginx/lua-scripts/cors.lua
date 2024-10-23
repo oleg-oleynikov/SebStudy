e9315d56ddb71f0ca3e313f705c2e25738084158
Олег Олейников <olegoleynikov234@gmail.com>
2024-10-23 18:13:14 +0300

    change in config

---
+++ nginx/lua-scripts/cors.lua
@@ -0,0 +1,23 @@
+return function(origin)
+    if not origin then
+      origin = "*"
+    end
+  
+    -- Pre-flight response
+    if ngx.req.get_method() == "OPTIONS" then
+      ngx.header["Access-Control-Allow-Origin"] = origin
+      ngx.header["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
+      ngx.header["Access-Control-Allow-Headers"] = "Keep-Alive,Cache-Control,Content-Type,Content-Transfer-Encoding,X-User-Agent,X-Grpc-Web"
+      ngx.header["Access-Control-Max-Age"] = 1728000
+      ngx.header["Content-Type"] = "text/plain; charset=utf-8"
+      ngx.header["Content-Length"] = 0
+      ngx.exit(ngx.HTTP_NO_CONTENT)
+      return
+    end
+  
+    -- Actual POST request
+    ngx.header["Access-Control-Allow-Origin"] = origin
+    ngx.header["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
+    ngx.header["Access-Control-Allow-Headers"] = "Keep-Alive,Cache-Control,Content-Type,Content-Transfer-Encoding,X-User-Agent,X-Grpc-Web"
+    ngx.header["Access-Control-Expose-Headers"] = "X-User-Agent,X-Grpc-Web,Grpc-Message,Grpc-Status"
+  end
